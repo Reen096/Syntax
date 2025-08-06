0fadda79d798d384d46c1cee211a84d3c2277ff1
Reen096 <dangnhatrin90@gmail.com>
2025-08-06 13:06:37 +0700

    Add files via upload

---
+++ Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+/**
+ * 
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Dang Nhat";
+		String lastName = "Rin";
+		int old = 29;
+		float markMath = 9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+
+		// Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+}
