838661c1ccd411b3dc4dce05f02e022c2abe5dca
Reen096 <dangnhatrin90@gmail.com>
2025-08-09 16:25:17 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,48 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// Thong tin mot sinh vien
+		String tenSinhVien = "Dang Nhat Rin";
+		String maSinhVien = "SV843";
+		int chungMinhNhanDan = 48384347;
+		String tenLop = "CNTT01";
+		String tenTruong = "THPT Phan Dang Luu";
+		double diemTrungBinh = 6.5;
+
+		// Thong tin cong nhan
+		String tenCongNhan = "Nguyen Van A";
+		String diaChi = "06 Le Loi, Hue";
+		int soDienThoai = 754837584;
+		double heSoLuong = 2.5;
+
+		// Thong tin chiec xe
+		String tenXe = "Ferrari";
+		String hangXe = "Mercedes";
+		double giaXe = 47834783;
+		String mauSon = "den";
+
+		// In thong tin sinh vien
+		System.out.println("Tên: " + tenSinhVien);
+		System.out.println("Mã Sinh Viên: " + maSinhVien);
+		System.out.println("Chứng minh nhân dân: " + chungMinhNhanDan);
+		System.out.println("Tên lớp: " + tenLop);
+		System.out.println("Tên trường: " + tenTruong);
+		System.out.println("Điểm trung bình: " + diemTrungBinh);
+
+		// In ra thong tin cong nhan
+		System.out.println("Tên công nhân: " + tenCongNhan);
+		System.out.println("Địa chỉ: " + diaChi);
+		System.out.println("Số điện thoại: " + soDienThoai);
+		System.out.println("Hệ số lương: " + heSoLuong);
+
+		// Thong tin chiec xe
+		System.out.println("Tên xe: " + tenXe);
+		System.out.println("Hãng xe: " + hangXe);
+		System.out.println("Giá xe: " + giaXe);
+		System.out.println("Màu sơn: " + mauSon);
+
+	}
+
+}
